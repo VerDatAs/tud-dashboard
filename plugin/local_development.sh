da10afc09e3114bd0b42881fe1045ddcff498644
Tommy Kubica <tommy.kubica@tu-dresden.de>
2023-09-25 17:13:16 +0200

    Comment out copy script for lang folder, as it currently does not exist;

--- plugin/local_development.sh
+++ plugin/local_development.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -r ./classes/* ../../ilias/Customizing/global/plugins/Services/COPage/PageComponent/VerDatAsDsh/classes/
-cp -r ./lang/* ../../ilias/Customizing/global/plugins/Services/COPage/PageComponent/VerDatAsDsh/lang/
+# cp -r ./lang/* ../../ilias/Customizing/global/plugins/Services/COPage/PageComponent/VerDatAsDsh/lang/
 cp -r ./templates/* ../../ilias/Customizing/global/plugins/Services/COPage/PageComponent/VerDatAsDsh/templates/
