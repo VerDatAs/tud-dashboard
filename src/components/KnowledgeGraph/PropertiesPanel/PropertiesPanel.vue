54c7578df39a4bb515376c32e914c64bcaa85058
Tommy Kubica <tommy.kubica@tu-dresden.de>
2023-10-05 15:22:45 +0200

    Add markRaw also for referencedTests to make sure that similar issues as observed for priorKnowledge do not occur;

--- src/components/KnowledgeGraph/PropertiesPanel/PropertiesPanel.vue
+++ src/components/KnowledgeGraph/PropertiesPanel/PropertiesPanel.vue
@@ -45,7 +45,7 @@ export default {
           this.parametrizedElement?.businessObject?.referencedTests?.map(
             (referencedTest) => referencedTest?.elementId
           ) ?? []
-        this.referencedTestValue = this.allTests.filter((element) => referencedTestElements.includes(element.objectId))
+        this.referencedTestValue = markRaw(this.allTests.filter((element) => referencedTestElements.includes(element.objectId)))
       }
     }
   },
@@ -149,7 +149,7 @@ export default {
     },
     // Update the referenced test multiselect value
     updateSelectedReferencedTest(selectedElements, parameterName) {
-      this.referencedTestValue = selectedElements
+      this.referencedTestValue = markRaw(selectedElements)
       const referencedTestElements = []
       this.referencedTestValue.forEach((elem) => {
         const element = this.diagram.get('moddle').create('verDatAs:ReferencedTest', {
