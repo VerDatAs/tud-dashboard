98f07d173c90afbc7024a27ed29c9cbe5464c216
Niklas Harbig <niklas.harbig@outlook.de>
2023-10-13 12:36:45 +0200

    add font awesome icons

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ import App from './App.vue'
 
 import './assets/main.scss'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faMaximize, faBars, faRefresh, faFloppyDisk  } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faMaximize, faBars, faRefresh, faFloppyDisk)
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
