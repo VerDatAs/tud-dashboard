4717928fbfa3e5f83736017c1ce603e83736ce19
Max Schaible <max.schaible@mailbox.tu-dresden.de>
2023-12-07 16:42:46 +0100

    take graphData in init and trigger event in js

--- src/moodle.ts
+++ src/moodle.ts
@@ -4,10 +4,16 @@ import App from './App.vue'
 
 import './assets/main.scss'
 
-export function init() {
+export function init(initGraphData: Array<any>) {
+
   const app = createApp(App)
 
   app.use(createPinia())
 
   app.mount('#dashboardApp')
+
+  setTimeout(() => {
+    document.dispatchEvent(new CustomEvent("init-graph", { "detail": initGraphData }));
+  }, 1000);
 }
+
