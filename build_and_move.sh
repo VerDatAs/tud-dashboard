a290a307121216213ee1d0fbf90c4bfe6783bfae
Tommy Kubica <tommy.kubica@tu-dresden.de>
2024-02-24 14:42:18 +0100

    Reduce size of logo that is visible when loading the dashboard; Adjust type of the image;

--- build_and_move.sh
+++ build_and_move.sh
@@ -4,7 +4,7 @@ if [ "$1" == "ilias" ]; then
     npm i
     npm run build
     cp dist/assets/main.js ../verdatasdsh/templates/assets/main.js
-    cp dist/logo.jpeg ../verdatasdsh/templates/logo.jpeg
+    cp dist/logo.jpg ../verdatasdsh/templates/logo.jpg
     cp dist/tpl.content.html ../verdatasdsh/templates/tpl.content.html
     cd ../ilias-dev && sh reload_directories_without_composer_du.sh
 elif [ "$1" == "moodle" ]; then
